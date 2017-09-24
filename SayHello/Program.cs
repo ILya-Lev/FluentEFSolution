3f68c97ac2d5efbfefdc2a15cc3109e324026983
ILya-Lev <lis2889@gmail.com>
2017-09-24 17:43:47 +0300

    Add project files.

---
+++ SayHello/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace SayHello
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestModel();
+		}
+
+		private static void TestModel()
+		{
+			using (var context = new FluentEFChapter01Entities())
+			{
+				var dataToPrint = context.Recipes.Select(r => new { r.Title, r.RecipeType.Description }).ToList();
+				foreach (var item in dataToPrint)
+				{
+					Console.WriteLine($"{item.Title} {item.Description}");
+				}
+			}
+		}
+	}
+}
