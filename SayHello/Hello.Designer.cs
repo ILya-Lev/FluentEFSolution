fe26e5a3b55369aab0362bb18e8c0e1a219206a5
ILya-Lev <lis2889@gmail.com>
2017-09-25 21:31:59 +0300

    playing with the model

--- SayHello/Hello.Designer.cs
+++ SayHello/Hello.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'L:\programming\study\entity_framework\FluentEntityFramework\FluentEFSolution\SayHello\Hello.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Projects\LIS\studying\pet_projects\EntityFramework\SayHello\Hello.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
