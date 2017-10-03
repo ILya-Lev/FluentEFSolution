9c7bfe78186025b3be6a71aa2f0cb76f12733d1d
ILya-Lev <lis2889@gmail.com>
2017-10-03 15:08:21 +0300

    aquision with extending partitioned generated class

---
+++ ContextPreview/AmountPart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ContextPreview
+{
+	public partial class Amount
+	{
+		public void OnMaximumAmountChanged()
+		{
+			Console.WriteLine("maximum changed");
+		}
+
+		public string DisplayAmount =>
+			$"{MinimumAmount} to {MaximumAmount} {Unit}, "
+			+ $"{Recipe.RecipeIngredients.FirstOrDefault(i => i.RecipeID == RecipeID)?.Preparation}";
+
+	}
+}
