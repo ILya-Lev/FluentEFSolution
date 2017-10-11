76b3cea6b80161bbf720ab899d6c422c36cf91ad
ILya-Lev <lis2889@gmail.com>
2017-10-11 19:50:23 +0300

    db first simple recipe project added with entities creation samples

---
+++ SimpleRecipes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleRecipes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var context = new RecipesContext())
+			{
+				var parentRecipe = new Recipe { RecipeName = "another parent recipe" };
+				var parentIngredient = new RecipeIngredient { Preparation = "parent's ingredient" };
+				parentRecipe.RecipeIngredients.Add(parentIngredient);
+
+				var childRecipe = new Recipe { RecipeName = "child recipe" };
+				var childIngredient = new RecipeIngredient { Preparation = "child's ingredient" };
+				childRecipe.RecipeIngredients.Add(childIngredient);
+
+
+				context.Recipes.Add(parentRecipe);
+				context.RecipeIngredients.Add(childIngredient);
+				context.SaveChanges();
+			}
+
+		}
+	}
+}
